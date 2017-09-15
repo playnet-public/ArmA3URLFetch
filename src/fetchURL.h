04301faca6014a381c47e4b8e034f2a78eb304f1
Vincent <agpheaddikilla@yahoo.de>
2017-09-15 18:59:29 +0200

    Changed fetchURL.h

--- src/fetchURL.h
+++ src/fetchURL.h
@@ -13,16 +13,12 @@ class FetchURL
 {
 public:
     void callExtension (const char * output, int outputSize, const char * function);
-private:
     std::mutex fResults_lock;
     std::vector<FetchResult> fResults;
-    
     void startGETThread(std::string function, std::string parameters);
     void startPOSTThread(std::string function, std::string parameters);
-
     void fetchResultGET(std::string * function, std::string * parameters);
     void fetchResultPOST(std::string * function, std::string * parameters);
-
     int returnStatus(int key);
     std::string returnResult(int key);
 };
